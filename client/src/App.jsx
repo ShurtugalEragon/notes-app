dc9080a7fefc311b27c54a1065de8eacf3ef5d25
Ismael Suarez <qae20@txstate.edu>
2025-10-02 12:54:32 -0500

    Added save functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,17 @@ async function getDocument() {
   return data.content;
 }
 
+async function saveDocument(text) {
+  let textObject = {content: text};
+  await fetch("http://127.0.0.1:8080/docs/1", {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(textObject)
+  });
+}
+
 function App() {
   const [documentText, setDocumentText] = useState('');
 
@@ -24,7 +35,12 @@ function App() {
   }, []);
 
   return (
-    <textarea value={documentText} onChange={e => setDocumentText(e.target.value)}></textarea>
+    <>
+    <div>
+      <textarea value={documentText} onChange={e => setDocumentText(e.target.value)}></textarea>
+    </div>
+    <button onClick={() => {saveDocument(documentText)}}>Save</button>
+    </>
   );
 }
 
